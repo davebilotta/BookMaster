4cda5da58d5e5c306cdf1dee8dc7d28f073d5a27
Dave Bilotta <davebilotta@gmail.com>
2016-02-16 12:20:06 -0500

    Book entry/layout

---
+++ app/src/main/java/bookmaster/davebilotta/com/bookmaster/BookEntry.java
@@ -0,0 +1,81 @@
+package bookmaster.davebilotta.com.bookmaster;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+/**
+ * Created by Dave on 2/16/2016.
+ *
+ * This is ued to create a new Book (manual entry) or update an existing entry
+ */
+public class BookEntry extends Activity {
+
+    EditText title,desc,authors,year,publisher, isbn;
+    Button cancel,save;
+    DB db;
+    Book book;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.book_entry);
+        db = new DB(this);
+        db.open();
+
+        //book = new Book();
+
+        // EditText components
+        title = (EditText) findViewById(R.id.titleText);
+
+        // Button components
+        cancel = (Button) findViewById(R.id.cancelButton);
+        cancel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(BookEntry.this, ViewList.class);
+                startActivity(i);
+            }});
+
+        save = (Button) findViewById(R.id.saveButton);
+        save.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                    saveEntry();
+            }
+        });
+    }
+
+    public void saveEntry() {
+        //String title, String desc, String authors, String year, String publisher, String isbn) {
+        try {
+            db.insertEntry(title.getText().toString(),
+                    desc.getText().toString(),
+                    authors.getText().toString(),
+                    year.getText().toString(),
+                    publisher.getText().toString(),
+                    isbn.getText().toString()
+            );
+
+            db.close();
+
+            // Now clear everything
+            title.setText("");
+            isbn.setText("");
+            desc.setText("");
+            authors.setText("");
+            year.setText("");
+            publisher.setText("");
+
+            // TODO: Show Snackbar so user knows item was saved and they can keep adding more
+            // TODO: Allow them to Undo?
+        }
+        catch (Exception e) {
+            Utils.log(e.getMessage());
+        }
+
+    }
+}
--- app/src/main/java/bookmaster/davebilotta/com/bookmaster/DB.java
+++ app/src/main/java/bookmaster/davebilotta/com/bookmaster/DB.java
@@ -28,13 +28,13 @@ public class DB {
             db = helper.getWritableDatabase();
         }
         catch (SQLiteException e) {
-            Utils.log("Open Exception Caught",e.getMessage();
+            Utils.log("Open Exception Caught",e.getMessage());
             db = helper.getReadableDatabase();
         }
     }
 
     // Call this, passing in a book that would have been created on the edit/create screen
-    public long insertEntry(Book book) {
+    /*public long insertEntry(Book book) {
         try {
             ContentValues entry = new ContentValues();
             entry.put(DBShared.ITEM_TITLE,book.getTitle());
@@ -52,16 +52,38 @@ public class DB {
             Utils.log("Insert Exception found",e.getMessage());
             return -1;
         }
+    } */
+
+    public long insertEntry(String title, String desc, String authors, String year, String publisher, String isbn) {
+        try {
+            ContentValues entry = new ContentValues();
+            entry.put(DBShared.ITEM_TITLE, title);
+            entry.put(DBShared.ITEM_COUNT, 1);
+            entry.put(DBShared.ITEM_DESC, desc);
+
+            // TODO: Figure this out - how will multiples be returned by api?
+            entry.put(DBShared.ITEM_AUTHORS, authors);
+            entry.put(DBShared.ITEM_YEAR, year);
+            entry.put(DBShared.ITEM_PUBLISHER, publisher);
+            entry.put(DBShared.ITEM_ISBN, isbn);
+
+            entry.put(DBShared.CREATE_DATE, System.currentTimeMillis());
+
+            return db.insert(DBShared.TABLE_NAME, null, entry);
+        } catch (SQLiteException e) {
+            Utils.log("Insert Exception found", e.getMessage());
+            return -1;
+        }
     }
 
     public Cursor getEntries() {
         Cursor c = db.query(DBShared.TABLE_NAME,null,null,null,null,null,null);
         return c;
     }
-    
+
     public int getNextID() {
         // TODO: Fix this later - for now just return the time in ms
-        return (int)java.lang.System.currentTimeMillis();
+        return (int) System.currentTimeMillis();
     }
 
 
